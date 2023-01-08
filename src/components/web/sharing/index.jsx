f0d14f0534c9542eff785027cb4acca7b1390e56
Lo Leon <lonelyliar8523@gmail.com>
2023-01-09 00:24:26 +0800

    Update index.jsx

--- src/components/web/sharing/index.jsx
+++ src/components/web/sharing/index.jsx
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class Sharing extends Component {
     render() {
         return (
             <div className="sharing">
                 If you like it, please give me a <iframe
-                    src="https://ghbtns.com/github-btn.html?user=LonelyLiaR&amp;repo=R2048&amp;type=star&amp;count=false"
+                    src="https://ghbtns.com/github-btn.html?user=glitchboyl&amp;repo=R2048&amp;type=star&amp;count=false"
                     title='star'
                     frameBorder="0"
                     scrolling="0"
