40b595a11e1deb45988e2d9ea44b6b152a0959e6
Lo Leon <lonelyliar8523@gmail.com>
2023-01-09 00:25:05 +0800

    Update index.jsx

--- src/components/web/note/index.jsx
+++ src/components/web/note/index.jsx
@@ -4,7 +4,7 @@ export default class Note extends Component {
     render() {
         return (
             <p className="note">
-                <strong className="important">Note:</strong> The game on this site is power by <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a>. Apps for <a href="#" target="_blank" rel="noopener noreferrer">Android</a> are also available. Created by <a href="https://github.com/LonelyLiaR" target="_blank" rel="noopener noreferrer">LonelyLiaR</a>. Inspired by <a href="https://gabrielecirulli.github.io/2048/" target="_blank" rel="noopener noreferrer">Gabriele Cirulli</a>.
+                <strong className="important">Note:</strong> The game on this site is power by <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a>. Apps for <a href="#" target="_blank" rel="noopener noreferrer">Android</a> are also available. Created by <a href="https://github.com/glitchboyl" target="_blank" rel="noopener noreferrer">glitchboyl</a>. Inspired by <a href="https://gabrielecirulli.github.io/2048/" target="_blank" rel="noopener noreferrer">Gabriele Cirulli</a>.
             </p>
         )
     }
